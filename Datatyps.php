5aeab604fb7c7b54d8b2e3258fbc7f26bd7937d3
Krushika <137705754+krushika-25@users.noreply.github.com>
2023-07-14 11:52:29 +0530

    Add files via upload

---
+++ Datatyps.php
@@ -0,0 +1,32 @@
+<?php
+$x = "Hello World";
+$y = "Hello world";
+
+echo $x;
+echo "<br>";
+echo $y;
+
+$a = 5989;
+var_dump($a);
+
+$b = 10.365;
+var_dump($b);
+
+$cars = array("Volvo","BMW","Toyota");
+var_dump($cars);
+
+$a1 = "Hello world!";
+$a1 = null;
+var_dump($a1);
+
+$b = 10.365;
+var_dump($b);
+
+$cars = array("Volvo","BMW","Toyota");
+var_dump($cars);
+
+$a1 = "Hello world";
+$a1 = null;
+var_dump($a1);
+
+?>
---
+++ phpprogram1.php
@@ -0,0 +1,31 @@
+<?php
+    $silveroak="Hello";
+    $a=20;
+    $b="How are you?";
+
+    $x = 10;
+    $y = 20;
+    $sum = $x + $y;
+    $sub = $x - $y;
+    $mul = $x * $y;
+    $div = $x / $y;
+    $div1 = $y / $x;
+    $div2 = (float) ($x/$y);
+
+
+    echo $silveroak . "<br/>";
+    echo $a . "<br/>";
+    echo $b . "<br/>";
+    
+    
+    echo "sum=" . $sum . "<br/>";
+    echo "sub=" . $sub . "<br/>";
+    echo "mul=" . $mul . "<br/>";
+    echo "div=" . $div . "<br/>";
+    echo "div1=" . $div1 . "<br/>";
+    echo "div2=" . $div2 . "<br/>";
+
+    var_dump($sum) . "<br/>";
+    var_dump("Heloooo") . "<br/>";
+
+?>
